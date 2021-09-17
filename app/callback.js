de90a56fd93ac0c8d0ec39e864c12c33bb5e9811
mandeep <singh.mr.1996@gmail.com>
2021-09-17 13:39:03 +0530

    Refacter: .env and database.config

--- app/callback.js
+++
@@ -1,7 +0,0 @@
-function write(callback) {
-  var a = 100;
-  callback(a);
-}
-var a = () => {
-  console.log("total : " + a);
-};
--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,20 @@
-module.exports = {
-  url: "mongodb://localhost:27017/FundooNotes",
-};
+const mongoose = require("mongoose");
+const url = process.env.URL;
+class DbConnection {
+  database = () => {
+    mongoose.Promise = global.Promise;
+    // Connecting to the database
+    mongoose
+      .connect(url, {
+        useNewUrlParser: true,
+      })
+      .then(() => {
+        console.log("Successfully connected to the database");
+      })
+      .catch((err) => {
+        console.log("Could not connect to the database. Exiting now...", err);
+        process.exit();
+      });
+  };
+}
+module.exports = new DbConnection();
